6b9e8e19bcd94ca1b00953a007d1a7aaab85cdc8
Dependability <Robloxplayer307@gmail.com>
2022-12-05 22:41:41 -0600

    Multiple functional additions, also some very bland developmental css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import Task from './modules/task.js';
 import DOMUpdater from './modules/dom.js';
 const projects = [];
 let currentProj = '';
+const taskWrap = document.querySelector('.task-view-wrapper');
+function selectProject(index) {
+    
+    const DOMUpdate = new DOMUpdater();
+    if (index === undefined) {
+        DOMUpdate.noTasks();
+        return;
+    }
+    
+    currentProj.button.classList.remove('selected');
+    currentProj = projects[index];
+    DOMUpdate.showCurrentProject(currentProj);
+    currentProj.button.classList.add('selected');      
+    
+   
+}
+
+function removeProject(e) {
+    e.stopPropagation();
+        //delete the project
+
+        const projectIndex = findProject(e.target.getAttribute('data-key'));
+        const removed = projects.splice(projectIndex, 1);
+        console.log(removed)
+        removed[0].button.remove();
+        console.log(projects)
+
+        if (removed[0] === currentProj) {
+            if (projects.length > 0) {
+                selectProject(projectIndex - 1);
+                
+            } else {
+                console.log("you got me")
+                selectProject();
+            }
+        } 
+}
 
 function firstLaunch() {
     const firstProject = new Project("Default Project");
@@ -10,39 +47,84 @@ function firstLaunch() {
     firstProject.addTask(firstTask);
     const DOMUpdate = new DOMUpdater();
     projects.push(firstProject)
-    DOMUpdate.addProject(firstProject)
-    currentProj = firstProject;
-    DOMUpdate.showCurrentProject(firstProject);
+    const button = DOMUpdate.addProject(firstProject)['projectButton']
+    currentProj = projects[projects.length - 1];
+    DOMUpdate.showCurrentProject(currentProj);
+
+    
+    currentProj.button = button;
+    currentProj.button.classList.add('selected');
+    button.querySelector('button').addEventListener('click', removeProject);
+    button.addEventListener('click', (e) => {
+        const projectNum = findProject(e.target.getAttribute('data-key'));
+        if (projectNum == -1) {
+            console.log("Invalid name, debug")
+            console.log(e.target.textContent)
+            return;
+        }
+        selectProject(projectNum);
+    });
+}
+
+function findProject(name) {
+    for (let i = 0; i < projects.length ; i++ ) {
+        if (projects[i].name == name) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 firstLaunch();
 
 const projectConfirmBut = document.querySelector(".addprojectdiv .confirm");
+const projectCancelBut = document.querySelector('.addprojectdiv .cancel');
 const projectNameField = document.querySelector ("input#add-project-text");
 const projectDiv = document.querySelector("div.addprojectdiv");
+
 const taskDiv = document.querySelector('div.addtasksdiv')
 
 const taskbut = document.querySelector("button.taskbut");
 taskbut.addEventListener("click", () => {
-    taskDiv.classList.toggle("invisible");
+    taskDiv.classList.remove("invisible");
 });
 const projbut = document.querySelector("button.projbut");
 projbut.addEventListener("click", (e) => {
-    projectDiv.classList.toggle("invisible");
+    projectDiv.classList.remove("invisible");
 });
 
 
 projectConfirmBut.addEventListener('click', () => {
-    projectDiv.classList.toggle("invisible");
+    projectDiv.classList.add("invisible");
     const newestProj = new Project(projectNameField.value)
     projects.push(newestProj)
     projectNameField.value = '';
     const DOMUpdate = new DOMUpdater();
-    DOMUpdate.addProject(newestProj);
-    projects.push(newestProj)
-    currentProj = newestProj;
+    const projectButton = DOMUpdate.addProject(newestProj)['projectButton'];
+    currentProj.button.classList.remove('selected');
+    currentProj = projects[projects.length - 1];
+    currentProj.button = projectButton;
+    currentProj.button.classList.add('selected');
     DOMUpdate.showCurrentProject(currentProj);
+    console.log(projectButton);
 
+    projectButton.querySelector('button').addEventListener('click', removeProject);
+
+    projectButton.addEventListener('click', (e) => {
+        const projectNum = findProject(e.target.getAttribute('data-key'));
+        if (projectNum == -1) {
+            console.log("Invalid name, debug")
+            return;
+        }
+        
+
+        selectProject(projectNum);
+    })
+});
+
+projectCancelBut.addEventListener('click', ()=> {
+    projectNameField.value = '';
+    projectDiv.classList.add('invisible')
 });
 
 
@@ -53,7 +135,8 @@ const taskDate = document.querySelector("#add-task-date");
 const taskConfirm = document.querySelector('.addtasksdiv .confirm');
 
 taskConfirm.addEventListener("click", () => {
-    taskDiv.classList.toggle("invisible");
+    
+    taskDiv.classList.add("invisible");
     const newestTask = new Task(taskTitle.value, taskDesc.value, taskDate.value, taskPrior.value);
     taskTitle.value = '';
     taskDesc.value = '';
@@ -63,8 +146,24 @@ taskConfirm.addEventListener("click", () => {
 
     currentProj.addTask(newestTask);
     const DOMUpdate = new DOMUpdater();
-    DOMUpdate.showCurrentProject(currentProj)
+    DOMUpdate.addTaskToDom(newestTask);
 
 
 
 });
+
+
+
+taskWrap.addEventListener('click', ()=> {
+    taskWrap.classList.add('invisible');
+    console.log('done')
+})
+
+const taskView = document.querySelector('.task-view');
+taskView.addEventListener('click', (e)=>{
+    e.stopPropagation();
+} )
+
+taskView.querySelector('button').addEventListener('click', ()=> {
+    taskWrap.classList.add('invisible');
+})
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,41 +3,81 @@ class DOMUpdater {
     resetAll() {
         const tasks = document.querySelector('ol.tasklist');
         const projects = document.querySelector('ol.project-list');
-
+        const projectName = document.querySelector('.project-name');
         tasks.innerHTML = '';
+        
+    }
+
+    viewTask(task) {
+        const taskWrap = document.querySelector('.task-view-wrapper');
+        const taskView = document.querySelector('.task-view');
+
+        taskView.querySelector('h2').textContent = task.title;
+        taskView.querySelector('.task-description').textContent = task.description;
+        taskView.querySelector('.task-priority').textContent = task.priority;
+        taskView.querySelector('.due-date').textContent = task.dueDate;
+
+        taskWrap.classList.remove('invisible')
 
     }
 
+
     addTaskToDom(task) {
+
         const taskLi = document.createElement('li');
+        const taskLeft = document.createElement('div');
+        taskLi.classList.add('task-item');
+        taskLeft.classList.add('left');
+
         const taskTitle = document.createElement('h3');
         const taskDesc = document.createElement('p');
         const taskDD = document.createElement('p');
         const taskPrior = document.createElement('p');
-        const taskC = document.createElement('p');
-
+        const taskC = document.createElement('input');
+        taskC.setAttribute('type', 'checkbox')
         taskTitle.textContent = task.title;
-        taskDesc.textContent = task.description;
+        taskDesc.textContent = task.description.length > 15 ? task.description.substring(0,15) + '...' : task.description;
         taskDD.textContent = task.dueDate;
-        taskC.textContent = task.completed;
+
+        task.completed ? taskC.setAttribute('checked', '') : '';
+        
         taskPrior.textContent = task.priority;
 
-        taskLi.appendChild(taskTitle);
-        taskLi.appendChild(taskDesc);
+        
+        // taskLi.appendChild(taskDesc);
+        taskLi.appendChild(taskLeft);
         taskLi.appendChild(taskDD);
-        taskLi.appendChild(taskC);
-        taskLi.appendChild(taskPrior);
+        taskLeft.appendChild(taskC);
+        taskLeft.appendChild(taskTitle);
+        taskLeft.appendChild(taskDesc)
+        // taskLi.appendChild(taskPrior);
 
         const tasks = document.querySelector('ol.tasklist');
         tasks.appendChild(taskLi);
 
+        taskLi.addEventListener('click', () => {
+            console.log(task.description);
+            this.viewTask(task)
+        })
+
+        taskC.addEventListener('click', (e) => e.stopPropagation());
+        taskC.addEventListener('change', (e) => {
+            task.completed = e.target.checked;
+        //     task.description
+        })
+
     } 
 
     showCurrentProject(project) {
+        document.querySelector('.tasks-side').classList.remove('nodisplay');
         this.resetAll();
+        
+
         project.tasks.forEach(element => {
             this.addTaskToDom(element)
         });
+        const projectTitle = document.querySelector('.tasks-side .project-name');
+        projectTitle.textContent = `Project: ${project.name}`
 
 
     }
@@ -46,10 +86,21 @@ class DOMUpdater {
         const projectList = document.querySelector("ol.project-list");
         const projectLi = document.createElement("li");
         const projectName = document.createElement("button");
+        const deleteProject = document.createElement("button");
+        deleteProject.textContent = "X";
+        deleteProject.setAttribute('data-key', project.name);
 
         projectName.textContent = project.name;
+        projectName.setAttribute('data-key', project.name);
+        projectName.appendChild(deleteProject);
         projectLi.appendChild(projectName);
         projectList.appendChild(projectLi);
+
+        return {projectButton : projectName}
+    }
+
+    noTasks() {
+        document.querySelector('.tasks-side').classList.add('nodisplay');
     }
 }
 
